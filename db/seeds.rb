caeec98ecb2db584c03ccc8fe90d296cbb9fdfda
Anna Blood <ab00721@my.westga.edu>
2024-02-19 18:36:01 -0500

    seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Restaurant.create(name: "Boca", location: "Sarasota", will_split: 22, wont_split: 1)
+Restaurant.create(name: "State Street", location: "Sarasota", will_split: 12, wont_split: 7)
+Restaurant.create(name: "Oak and Stone", location: "Sarasota", will_split: 8, wont_split: 1)
+Restaurant.create(name: "The Rosemary", location: "Sarasota", will_split: 0, wont_split: 12)
+Restaurant.create(name: "Rocco's Tacos", location: "Delray", will_split: 53, wont_split: 3)
+Restaurant.create(name: "Pizza Rustica", location: "Delray", will_split: 103, wont_split: 0)
+Restaurant.create(name: "Vic and Angelo's", location: "Delray", will_split: 4, wont_split: 15)
+Restaurant.create(name: "Benny's", location: "Lake Worth", will_split: 29, wont_split: 5)
