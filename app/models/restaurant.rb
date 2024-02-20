8cf25ceb546e8122b460f3e241db2ed7fdc8dff3
Anna Blood <ab00721@my.westga.edu>
2024-02-19 20:16:13 -0500

    Updated to show restaurants in table

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,9 @@
 class Restaurant < ApplicationRecord
+  def vote(split_check)
+    if split_check
+      increment!(:will_split)
+    else
+      increment!(:wont_split)
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  resources :restaurants
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :restaurants do
+    post 'vote', on: :member
+  end
 end
