b1d06d83623491e3e19fd30897c73e1efcbe508a
Anna Blood <ab00721@my.westga.edu>
2024-04-14 15:24:11 -0400

    Updated controller tests

--- test/controllers/restaurants_controller_test.rb
+++ test/controllers/restaurants_controller_test.rb
@@ -1,39 +1,87 @@
 require "test_helper"
 
 class RestaurantsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
   setup do
     @restaurant = restaurants(:one)
+    @vote = votes(:one)
+  end
+
+  test "should get index not logged in" do
+    get restaurants_url
+    assert_response :success
+  end
+
+  test "should redirect instead of get new" do
+    get new_restaurant_url
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should redirect instead of vote history" do
+    get votes_url
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should show restaurant not logged in" do
+    get restaurant_url(@restaurant)
+    assert_response :success
+  end
+
+  test "should redirect instead of edit" do
+    get edit_restaurant_url(@restaurant)
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should navigate to show from index not logged in" do
+    get restaurants_url
+    assert_response :success
+    assert_select "a[href=?]", restaurant_path(@restaurant), text: "Show" do
+      get restaurant_path(@restaurant)
+      assert_response :success
+    end
+  end
+
+  test "should navigate to back to index from show not logged in" do
+    get restaurant_url(@restaurant)
+    assert_response :success
+    assert_select "a[href=?]", restaurants_path, text: "Back to restaurants" do
+      get restaurants_path
+      assert_response :success
+    end
   end
 
   test "should get index" do
+    sign_in users(:one)
     get restaurants_url
     assert_response :success
   end
 
   test "should get new" do
+    sign_in users(:one)
     get new_restaurant_url
     assert_response :success
   end
 
+  test "should show vote history" do
+    sign_in users(:one)
+    get votes_url
+    assert_response :success
+  end
+
   test "should show restaurant" do
+    sign_in users(:one)
     get restaurant_url(@restaurant)
     assert_response :success
   end
 
   test "should get edit" do
+    sign_in users(:one)
     get edit_restaurant_url(@restaurant)
     assert_response :success
   end
 
-  test "should destroy restaurant" do
-    assert_difference("Restaurant.count", -1) do
-      delete restaurant_url(@restaurant)
-    end
-
-    assert_redirected_to restaurants_url
-  end
-
   test "should navigate to new from index" do
+    sign_in users(:one)
     get restaurants_url
     assert_response :success
     assert_select "a[href=?]", new_restaurant_path, text: "New restaurant" do
@@ -43,6 +91,7 @@ class RestaurantsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should navigate to show from index" do
+    sign_in users(:one)
     get restaurants_url
     assert_response :success
     assert_select "a[href=?]", restaurant_path(@restaurant), text: "Show" do
@@ -52,6 +101,7 @@ class RestaurantsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should navigate to edit from index" do
+    sign_in users(:one)
     get restaurants_url
     assert_response :success
     assert_select "a[href=?]", edit_restaurant_path(@restaurant), text: "Edit" do
@@ -61,6 +111,7 @@ class RestaurantsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should navigate to back to index from new" do
+    sign_in users(:one)
     get new_restaurant_url
     assert_response :success
     assert_select "a[href=?]", restaurants_path, text: "Back to restaurants" do
@@ -70,6 +121,7 @@ class RestaurantsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should navigate to back to index from show" do
+    sign_in users(:one)
     get restaurant_url(@restaurant)
     assert_response :success
     assert_select "a[href=?]", restaurants_path, text: "Back to restaurants" do
@@ -79,6 +131,7 @@ class RestaurantsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should navigate to edit from show" do
+    sign_in users(:one)
     get restaurant_url(@restaurant)
     assert_response :success
     assert_select "a[href=?]", edit_restaurant_path(@restaurant), text: "Edit this restaurant" do
@@ -88,6 +141,7 @@ class RestaurantsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should navigate to back to index from edit" do
+    sign_in users(:one)
     get edit_restaurant_url(@restaurant)
     assert_response :success
     assert_select "a[href=?]", restaurants_path, text: "Back to restaurants" do
@@ -97,6 +151,7 @@ class RestaurantsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should navigate to show from edit" do
+    sign_in users(:one)
     get edit_restaurant_url(@restaurant)
     assert_response :success
     assert_select "a[href=?]", restaurant_path(@restaurant), text: "Show this restaurant" do
--- test/controllers/votes_controller_test.rb
+++ test/controllers/votes_controller_test.rb
@@ -1,48 +1,57 @@
 require "test_helper"
 
 class VotesControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   setup do
     @vote = votes(:one)
   end
 
-  test "should get index" do
+  test "should redirect instead of index" do
     get votes_url
-    assert_response :success
+    assert_redirected_to new_user_session_path
   end
 
-  test "should get new" do
-    get new_vote_url
-    assert_response :success
-  end
-
-  test "should create vote" do
-    assert_difference("Vote.count") do
-      post votes_url, params: { vote: { restaurant_id: @vote.restaurant_id, user_id: @vote.user_id, vote: @vote.vote } }
-    end
-
-    assert_redirected_to vote_url(Vote.last)
+  test "should redirect instead of show" do
+    get vote_url(@vote)
+    assert_redirected_to new_user_session_path
   end
 
   test "should show vote" do
+    sign_in users(:one)
     get vote_url(@vote)
     assert_response :success
   end
 
   test "should get edit" do
+    sign_in users(:one)
     get edit_vote_url(@vote)
     assert_response :success
   end
 
   test "should update vote" do
+    sign_in users(:one)
     patch vote_url(@vote), params: { vote: { restaurant_id: @vote.restaurant_id, user_id: @vote.user_id, vote: @vote.vote } }
     assert_redirected_to vote_url(@vote)
   end
 
   test "should destroy vote" do
+    sign_in users(:one)
     assert_difference("Vote.count", -1) do
       delete vote_url(@vote)
     end
 
     assert_redirected_to votes_url
   end
+
+  test "should navigate to restaurants from votes" do
+    sign_in users(:one)
+    get votes_url
+    assert_response :success
+    assert_select "a[href=?]", restaurants_path, text: "Home" do
+      get restaurants_path
+      assert_response :success
+    end
+  end
+
 end
