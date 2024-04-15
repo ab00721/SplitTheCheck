e17f8fd6ad2f405597c12aeeb527c385880d2180
Anna Blood <ab00721@my.westga.edu>
2024-04-14 20:31:46 -0400

    FINAL SPRINT 2 COMMIT

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,26 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save user without email" do
+    user = User.new(password: "password")
+    assert_not user.save, "Email can't be blank"
+  end
+
+  test "should not save user without valid email" do
+    user = User.new(email: "user", password: "password")
+    assert_not user.save, "Email can't be invalid"
+  end
+
+  test "should not save user without password" do
+    user = User.new(email: "user@email.com")
+    assert_not user.save, "Password can't be blank"
+  end
+
+  test "should not save user with same email" do
+    user = User.new(email: "user@email.com", password: "password")
+    assert user.save, "Email saved"
+    user = User.new(email: "user@email.com", password: "password")
+    assert_not user.save, "Email already used"
+  end
+
 end
--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 class VoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = User.create(email: "user1@email.com", password: "password")
+    @restaurant = Restaurant.create(name: "test restaurant", location: "Test Location")
+  end
+
+  test "vote should be created" do
+    vote = Vote.new(user: @user, restaurant: @restaurant, vote: true)
+    assert vote.valid?
+  end
+
+  test "vote should not be created without user" do
+    vote = Vote.new(restaurant: @restaurant, vote: true)
+    assert_not vote.valid?
+  end
+
+  test "vote should not be created without restaurant" do
+    vote = Vote.new(user: @user, vote: true)
+    assert_not vote.valid?
+  end
+
 end
